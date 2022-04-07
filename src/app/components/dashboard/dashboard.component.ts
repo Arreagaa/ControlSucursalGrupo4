80a9b83a159f65d47b86359ec0f6aa98d2bb0a8f
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-04-07 00:59:11 -0600

    Arreglar Sucursales Empresa

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { SucursalesService } from 'src/app/services/sucursales.service';
+//SUCURSALES
+import { Sucursales } from 'src/app/models/sucursales.model';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [ SucursalesService, UsuarioService ]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public token;
 
-  ngOnInit(): void {
+  //SUCURSALES
+  public sucursalModelGet: Sucursales;
+  public sucursalModelPost: Sucursales;
+  public sucursalModelId: Sucursales;
+
+  constructor(private _sucursalesService: SucursalesService, private _usuarioService: UsuarioService) {
+    this.sucursalModelPost = new Sucursales('','', '','');
+    this.sucursalModelId = new Sucursales('','','','');
+    this.token = this._usuarioService.obtenerToken();
   }
 
+  ngOnInit(): void {
+    this.getSucursales();
+  }
+
+  getSucursales(){
+    this._sucursalesService.obtenerSucursales(this.token).subscribe(
+      (response) => {
+        this.sucursalModelGet = response.empleados;
+        console.log(response);
+        console.log(this.sucursalModelGet);
+      },
+      (error)=>{
+        console.log(<any>error)
+      }
+    )
+  }
+
+  getSucursalId(idSucursal){
+    this._sucursalesService.obtenerSucursalId(idSucursal, this._usuarioService.obtenerToken()).subscribe(
+      (response) => {
+        this.sucursalModelId = response.usuarios;
+        console.log(response);
+        console.log(this.sucursalModelId);
+      },
+      (error)=>{
+        console.log(<any>error)
+      }
+    )
+  }
+
+  postSucursales(){
+    this._sucursalesService.agregarSucursal(this.sucursalModelPost, this._usuarioService.obtenerToken()).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getSucursales();
+      },
+      (error)=>{
+        console.log(<any>error);
+
+      }
+    )
+  }
+
+  deleteSucursal(idSucursal) {
+    this._sucursalesService.eliminarSucursal(idSucursal, this._usuarioService.obtenerToken()).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getSucursales();
+      },
+      (error)=>{
+        console.log(<any>error);
+
+      }
+    )
+  }
+
+  putSucursales(){
+    this._sucursalesService.editarEmpresa(this.sucursalModelId, this._usuarioService.obtenerToken()).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getSucursales();
+      },
+      (error)=>{
+        console.log(<any>error);
+
+      }
+    )
+  }
 }
