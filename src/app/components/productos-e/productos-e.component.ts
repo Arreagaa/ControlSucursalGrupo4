a8eac0f544f4c1fc346548ef7500bbf1883fd3c0
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-04-20 19:55:17 -0600

    CRUD de Productos por Empresa

--- src/app/components/productos-e/productos-e.component.ts
+++ src/app/components/productos-e/productos-e.component.ts
@@ -1,15 +1,108 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { ProductosEmpresaService } from 'src/app/services/productos-empresa.service';
+import { ProductosEmpresa } from 'src/app/models/productosEmpresa.model';
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-productos-e',
   templateUrl: './productos-e.component.html',
-  styleUrls: ['./productos-e.component.scss']
+  styleUrls: ['./productos-e.component.scss'],
+  providers: [ ProductosEmpresaService, UsuarioService ]
 })
 export class ProductosEComponent implements OnInit {
 
-  constructor() { }
+  public token;
+
+  //PRODUCTOS DE LA EMPRESA
+  public productosEmpresaModelGet: ProductosEmpresa;
+  public productosEmpresaModelPost: ProductosEmpresa;
+  public productosEmpresaModelId: ProductosEmpresa;
+
+  constructor(private _productosEmpresaService: ProductosEmpresaService, private _usuarioService: UsuarioService) {
+    this.productosEmpresaModelPost = new ProductosEmpresa('','', '',0, '');
+    this.productosEmpresaModelId = new ProductosEmpresa('','','',0, '');
+    this.token = this._usuarioService.obtenerToken();
+  }
 
   ngOnInit(): void {
+    this.getProductosEmpresa();
+  }
+
+  getProductosEmpresa(){
+    this._productosEmpresaService.obtenerProductosEmpresa(this.token).subscribe(
+      (response) => {
+        this.productosEmpresaModelGet = response.productos;
+        console.log(response);
+        console.log(this.productosEmpresaModelGet);
+      },
+      (error)=>{
+        console.log(<any>error)
+      }
+    )
+  }
+
+  getProductosEmpresaId(idSucursal){
+    this._productosEmpresaService.obtenerProductosEmpresaId(idSucursal, this._usuarioService.obtenerToken()).subscribe(
+      (response) => {
+        this.productosEmpresaModelId = response.productos;
+        console.log(response);
+        console.log(this.productosEmpresaModelId);
+      },
+      (error)=>{
+        console.log(<any>error)
+      }
+    )
+  }
+
+  postProductosEmpresa(){
+    this._productosEmpresaService.agregarProductoEmpresa(this.productosEmpresaModelPost, this._usuarioService.obtenerToken()).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getProductosEmpresa();
+      },
+      (error)=>{
+        console.log(<any>error);
+        Swal.fire({
+          icon: 'warning',
+          title: 'Algo no anda bien...',
+          text: '¡Revisa que la información este correcta!',
+          footer: '<a>No dejes campos vacios, ¡gracias!</a>'
+        })
+      }
+    )
+  }
+
+  deleteProductosEmpresa(idSucursal) {
+    this._productosEmpresaService.eliminarProductosEmpresa(idSucursal, this._usuarioService.obtenerToken()).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getProductosEmpresa();
+      },
+      (error)=>{
+        console.log(<any>error);
+
+      }
+    )
+  }
+
+  putProductosEmpresa(){
+    this._productosEmpresaService.editarProductosEmpresa(this.productosEmpresaModelId, this._usuarioService.obtenerToken()).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getProductosEmpresa();
+      },
+      (error)=>{
+        console.log(<any>error);
+        Swal.fire({
+          icon: 'warning',
+          title: 'Algo no anda bien...',
+          text: '¡Revisa que la información este correcta!',
+          footer: '<a>No dejes campos vacios, ¡gracias!</a>'
+        })
+      }
+    )
   }
 
 }
---
+++ src/app/services/productos-empresa.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ProductosEmpresa } from '../models/productosEmpresa.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosEmpresaService {
+  public url: String = 'http://localhost:3000/api';
+  public headersVariable = new HttpHeaders().set('Content-Type','application/json');
+
+  constructor(public _http: HttpClient) { }
+
+  obtenerProductosEmpresa(token): Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token)
+
+    return this._http.get(this.url + '/obtenerProductos', { headers: headersToken })
+  }
+
+  agregarProductoEmpresa(modeloProductosEmpresa: ProductosEmpresa, token): Observable<any> {
+    let parametros = JSON.stringify(modeloProductosEmpresa);
+    let headersToken = this.headersVariable.set('Authorization', token);
+
+    return this._http.post(this.url + '/agregarProducto', parametros, {headers: headersToken})
+  }
+
+  eliminarProductosEmpresa(id : String, token): Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token);
+
+    return this._http.delete(this.url + '/eliminarProducto/' + id, { headers: headersToken })
+  }
+
+  obtenerProductosEmpresaId(id:String, token): Observable<any>{
+    let headersToken = this.headersVariable.set('Authorization', token);
+    return this._http.get(this.url + '/obtenerProductosId/' + id, {headers: headersToken})
+  }
+
+  editarProductosEmpresa(modeloProductosEmpresa: ProductosEmpresa, token): Observable<any> {
+    let parametros = JSON.stringify(modeloProductosEmpresa);
+    let headersToken = this.headersVariable.set('Authorization', token);
+
+    return this._http.put(this.url + '/editarProducto/' + modeloProductosEmpresa._id, parametros, { headers: headersToken })
+  }
+}
