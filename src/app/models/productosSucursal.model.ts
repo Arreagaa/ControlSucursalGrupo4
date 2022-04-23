f4510603facee8d09f272af3ecddc49844152f6f
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-04-23 00:22:39 -0600

    Arreglos Vistas

---
+++ src/app/models/productosSucursal.model.ts
@@ -0,0 +1,10 @@
+export class ProductosSucursal {
+  constructor(
+    public _id: String,
+    public nombreProductoSucursal: String,
+    public stockSucursal: Number,
+    public cantidadVendida: Number,
+    public idSucursal: String,
+    public idEmpresa: String
+  ){}
+}
---
+++ src/app/services/productos-sucursal.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ProductosSucursal } from '../models/productosSucursal.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosSucursalService {
+  public url: String = 'http://localhost:3000/api';
+  public headersVariable = new HttpHeaders().set('Content-Type','application/json');
+
+  constructor(public _http: HttpClient) { }
+
+  obtenerProductosSucursal(token): Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token)
+
+    return this._http.get(this.url + '/buscarProductoSucursal/', { headers: headersToken })
+  }
+}
